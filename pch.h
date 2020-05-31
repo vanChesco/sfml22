4cadf89dff91476ba3c78cc28d617f0c3ebd4c6c
vanchesco <vanchesco@icloud.com>
2020-05-31 21:59:56 +0200

    Dodaj pliki projektów.

---
+++ pch.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
---
+++ sfml22.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(400, 300), "ttt");
+
+    sf::Event event;
+
+    int i{ 0 }, a{ 1 };
+    int r{ 10 };
+
+    window.setFramerateLimit(240);
+
+    while (window.isOpen())
+    {
+        window.clear(sf::Color(0, 255, 255));
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        sf::CircleShape circle(r);
+        circle.setFillColor(sf::Color::Black);
+        circle.setOrigin(r, r);
+        circle.setPosition(100, float(i+1));
+        window.draw(circle);
+        if (i > (300 - r) || i < (0)) a=-a;
+        i = a*(i+1);
+
+        window.display();
+    }
+}
